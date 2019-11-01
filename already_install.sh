6b5f9527770a52572a1543a4f92695c48739982a
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2019-11-01 18:57:07 +0530

    added inverted scrolling function

--- already_install.sh
+++ already_install.sh
@@ -3,7 +3,12 @@ sudo cp ./setLeft /usr/bin/setLeft
 sudo cp ./setRight /usr/bin/setRight
 sudo g++ setLeft.cc -o /usr/bin/setLeftinit
 sudo g++ setRight.cc -o /usr/bin/setRightinit
-sudo echo "gesture swipe left 3 setLeftinit" >> /etc/libinput-gestures.conf
-sudo echo "gesture swipe right 3 setRightinit" >> /etc/libinput-gestures.conf
+if [ "$1" == "--inverted" ]; then
+	sudo echo "gesture swipe left 3 setRightinit" >> /etc/libinput-gestures.conf
+	sudo echo "gesture swipe right 3 setLeftinit" >> /etc/libinput-gestures.conf
+else
+	sudo echo "gesture swipe left 3 setLeftinit" >> /etc/libinput-gestures.conf
+	sudo echo "gesture swipe right 3 setRightinit" >> /etc/libinput-gestures.conf
+fi
 libinput-gestures-setup autostart
 libinput-gestures-setup start
--- install.sh
+++ install.sh
@@ -4,5 +4,27 @@ sudo cp ./setLeft /usr/bin/setLeft
 sudo cp ./setRight /usr/bin/setRight
 sudo g++ setLeft.cc -o /usr/bin/setLeftinit
 sudo g++ setRight.cc -o /usr/bin/setRightinit
+if [ "$1" == "--inverted" ]; then
+	printf "
+	#syntax:
+	#gesture <action> <motion> <num-fingers> <command>
+	# where action and motion is either:
+	#     swipe up
+	#     swipe down
+	#     swipe left
+	#     swipe right
+	#     swipe left_up
+	#     swipe left_down
+	#     swipe right_up
+	#     swipe right_down
+	#     pinch in
+	#     pinch out
+	#     pinch clockwise
+	#     pinch anticlockwise
+	" > /etc/libinput-gestures.conf
+	sudo echo "gesture swipe left 3 setRightinit" >> /etc/libinput-gestures.conf
+	sudo echo "gesture swipe right 3 setLeftinit" >> /etc/libinput-gestures.conf
+	gesture swipe right 3 setLeftinit
+fi
 libinput-gestures-setup autostart
 libinput-gestures-setup start
