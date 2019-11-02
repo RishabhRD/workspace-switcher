b6862da7f9a9192175e3c31eb3dfe3385870d75a
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2019-11-02 07:09:00 +0530

    systemctl start worked here

--- cronSet.sh
+++
@@ -1,4 +0,0 @@
-crontab -l > ~/.mycron
-echo "@reboot /usr/bin/startSwitcher" >> ~/.mycron
-crontab ~/.mycron
-rm ~/.mycron
---
+++ workspace-switcher.cc
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+int main(){
+	system("/usr/bin/workspace-switcher-script");
+}
