f54306bd36940a067fca756040893794501a04af
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2019-11-02 07:32:20 +0530

    solved invert bug

--- already_install.sh
+++ already_install.sh
@@ -1,14 +1,15 @@
 sudo gpasswd -a $USER input
 sudo cp ./setLeft /usr/bin/setLeft
-sudo cp ./setRight /usr/bin/setRight
 sudo g++ setLeft.cc -o /usr/bin/setLeftinit
 sudo g++ setRight.cc -o /usr/bin/setRightinit
 if [ "$1" == "--inverted" ]; then
-	sudo echo "gesture swipe left 3 setRightinit" >> /etc/libinput-gestures.conf
-	sudo echo "gesture swipe right 3 setLeftinit" >> /etc/libinput-gestures.conf
+	echo "gesture swipe left 3 setRightinit" > .tmp
+	echo "gesture swipe right 3 setLeftinit" >> .tmp
+	sudo mv .tmp /etc/libinput-gestures.conf
 else
-	sudo echo "gesture swipe left 3 setLeftinit" >> /etc/libinput-gestures.conf
-	sudo echo "gesture swipe right 3 setRightinit" >> /etc/libinput-gestures.conf
+	sudo echo "gesture swipe left 3 setLeftinit" > .tmp
+	sudo echo "gesture swipe right 3 setRightinit" >> .tmp
+	sudo mv .tmp /etc/libinput-gestures.conf
 fi
 libinput-gestures-setup start
 libinput-gestures-setup autostart
--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 sudo gpasswd -a $USER input
 sudo make install
 sudo cp ./setLeft /usr/bin/setLeft
-sudo cp ./setRight /usr/bin/setRight
 sudo g++ setLeft.cc -o /usr/bin/setLeftinit
 sudo g++ setRight.cc -o /usr/bin/setRightinit
 if [ "$1" == "--inverted" ]; then
@@ -21,10 +20,10 @@ if [ "$1" == "--inverted" ]; then
 	#     pinch out
 	#     pinch clockwise
 	#     pinch anticlockwise
-	" > /etc/libinput-gestures.conf
-	sudo echo "gesture swipe left 3 setRightinit" >> /etc/libinput-gestures.conf
-	sudo echo "gesture swipe right 3 setLeftinit" >> /etc/libinput-gestures.conf
-	gesture swipe right 3 setLeftinit
+	" > .tmp
+	echo "gesture swipe left 3 setRightinit" >> .tmp
+	echo "gesture swipe right 3 setLeftinit" >> .tmp
+	sudo mv .tmp /etc/libinput-gestures.conf
 fi
 libinput-gestures-setup start
 libinput-gestures-setip autostart
